4b1ff20470a15478ce02337ee9121e4bf5c4569f
TipSegWit <tipsegwit@protonmail.com>
2017-04-11 11:31:39 +0200

    Modified webpack config to comply with standard rules

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,31 +1,31 @@
-import webpack from 'webpack';
+import webpack from 'webpack'
 
 export default {
   entry: `${__dirname}/src/index.js`,
   output: {
     path: `${__dirname}/build`,
     publicPath: '/build/',
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
-    ],
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' }
+    ]
   },
 
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['', '.js', '.jsx']
   },
 
   plugins: process.argv.indexOf('-p') === -1 ? null : [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new webpack.optimize.UglifyJsPlugin({
       output: {
-        comments: false,
-      },
-    }),
-  ],
-};
+        comments: false
+      }
+    })
+  ]
+}
